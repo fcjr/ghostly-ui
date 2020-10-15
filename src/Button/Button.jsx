305cb52949deae33e2378188ec5eda71fc584e2e
leuryr <leuryr@yahoo.com>
2020-10-15 18:28:59 -0400

    Add proptypes to Button component

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import style from './Button.module.scss';
 
 const Button = ({
-	autofocus = false,
-	disabled = false,
-	name = '',
-	value = '',
-	type = 'button',
+	autofocus,
+	disabled,
+	name,
+	value,
+	type,
 	text,
-	tier = 'primary',
+	tier,
 }) => (
 	<button
 		autoFocus={autofocus}
@@ -21,5 +22,44 @@ const Button = ({
 		{text}
 	</button>
 );
+
+Button.propTypes = {
+	/**
+	 * Whether the button should be autofocused
+	 */
+	autofocus: PropTypes.bool,
+	/**
+	 * Whether or not the button is in disabled state
+	 */
+	disabled: PropTypes.bool,
+	/**
+	 * The name attribute to be assigned to button
+	 */
+	name: PropTypes.string,
+	/**
+	 * The value attribute to be assigned to button
+	 */
+	value: PropTypes.string,
+	/**
+	 * The text shown inside the button
+	 */
+	text: PropTypes.string.isRequired,
+	/**
+	 * The type of button
+	 */
+	type: PropTypes.string,
+	/**
+	 * Determines button's color tier
+	 */
+	tier: PropTypes.string.isRequired,
+};
+
+Button.defaultProps = {
+	autofocus: false,
+	disabled: false,
+	name: '',
+	value: '',
+	type: 'button',
+};
 
 export default Button;
