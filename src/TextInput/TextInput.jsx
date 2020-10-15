7af12c50f8069289883bf97778f5e4fc6659ee1e
Frank Chiarulli Jr <frank@frankchiarulli.com>
2020-10-15 16:49:26 -0400

    more styling for input

--- src/TextInput/TextInput.jsx
+++ src/TextInput/TextInput.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
+import classNames from 'classnames';
 import style from './TextInput.module.scss';
 
 const TextInput = ({
-	type = 'text',
 	label,
 	placeholder,
 	value,
 	onChange,
-	helpText
-}) => (
-	<div>
-		{label && <label>{label}</label>}
-		<input
-			type={type}
-			className={style.input}
-			value={value}
-			onChange={(e) => onChange && onChange(e.target.value)}
-			placeholder={placeholder}
-		/>
-		{helpText && <div>{helpText}</div>}
-	</div>
-);
+	errorText
+}) => {
+	const inputClassnames = classNames(
+		style.input,
+		{
+			[style.error]: errorText
+		}
+	);
+	return (
+		<div className={style.container}>
+			{label && <label className={style.label}>{label}</label>}
+			<input
+				type="text"
+				className={inputClassnames}
+				value={value}
+				onChange={(e) => onChange && onChange(e.target.value)}
+				placeholder={placeholder}
+			/>
+			{errorText && <div className={style.errorText}>{errorText}</div>}
+		</div>
+	);
+};
 
 export default TextInput;
--- src/TextInput/TextInput.stories.jsx
+++ src/TextInput/TextInput.stories.jsx
@@ -3,21 +3,31 @@ import React from 'react';
 import TextInput from './TextInput';
 
 export default {
-	title: 'Inputs/FormInputs/TextInput',
+	title: 'Inputs/TextInput',
 	component: TextInput,
+	argTypes: {
+		label: { control: 'text' },
+		placeholder: { control: 'text' },
+		value: { control: 'text' },
+		errorText: { control: 'text' },
+		onChange: { action: 'changed' }
+	},
 };
 
 const Template = (args) => <TextInput {...args} />;
 
-export const Email = Template.bind({});
-Email.args = {
-	label: 'Email',
-	placeholder: 'name@email.com',
+export const Default = Template.bind({});
+Default.args = {};
+
+export const WithLabel = Template.bind({});
+WithLabel.args = {
+	label: 'email',
+	placeholder: 'name@example.com',
 };
 
-export const EmailPopulated = Template.bind({});
-EmailPopulated.args = {
-	label: 'Email',
-	placeholder: 'name@email.com',
-	value: 'email@example.com'
+export const Error = Template.bind({});
+Error.args = {
+	label: 'email',
+	placeholder: 'name@example.com',
+	errorText: 'Invalid Email',
 };
